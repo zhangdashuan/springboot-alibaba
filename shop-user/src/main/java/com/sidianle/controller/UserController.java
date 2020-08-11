a8dee9fa8c67598c87d9018f3573d0443479a745
zhangdashuan <3200241142@qq.com>
2020-08-11 23:13:58 +0800

    新增： 1. 年轻人的第一次git提交

--- shop-user/src/main/java/com/sidianle/controller/UserController.java
+++ shop-user/src/main/java/com/sidianle/controller/UserController.java
@@ -1,4 +1,18 @@
 package com.sidianle.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/user")
 public class UserController {
+
+    @RequestMapping("/update")
+    public void update(@RequestParam("username") String username) {
+        System.out.println("插入一条用户信息");
+    }
+
 }
